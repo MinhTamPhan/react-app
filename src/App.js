ac3cde86dc912a14eed4977416213682db44ab27
MINHTAM\MinhTam <phanminhtam247@gmail.com>
2017-11-29 22:54:40 +0700

    first game

--- src/App.js
+++ src/App.js
@@ -1,58 +1,144 @@
 import React, { Component } from 'react';
+import './App.css';
 
-
-function Bird(props) {
+function Cell(props){
   var style = {
-    position: 'absolute',
-    top: props.bird.top,
-    left: props.bird.left,
-    width: props.bird.width,
-    height: props.bird.height,
-    border: '3px solid black',
-    backgroundColor: 'red'
+    width: 20,
+    height: 20,
+    backgroundColor: props.cell,
+    border: '1px solid black'
+  }
+  return <div style = {style}/>;
+}
+
+function Row(props) {
+  var style = {
+    display: 'flex'
   }
   return (
-    <div style = {style}/>
+    <div style = {style}>
+      {
+        props.row.map( (cell, index)=>{
+          return <Cell cell = {cell} key={index}/>
+        })
+      }
+    </div>
   )
 }
 
 function Gird(props) {
-  var style = {
-    width: props.gird.widthGird,
-    height:props.gird.heightGird,
-    backgroundColor: 'navy'
-  }
   return (
-    <div style = {style}>
-      <Bird bird = {props.bird}/>
+    <div>
+      {
+        props.gird.map( (row, index) => {
+          return <Row row = {row} key={index}/>
+        })
+      }   
     </div>
   )
 }
-class Game extends React.Component {
-  constructor (props) {
-    super(props);
 
-  
-      var gird = {
-        widthGird: 800,
-        heightGird: 660,
+class Game extends Component {
+  constructor(props) {
+    super(props);
+    var gird = [];
+    for (let i = 0; i < 30; i++) {
+      gird.push(new Array(60).fill('#8b9dc3'));
+    }
+    var bird = {
+      height: 10,
+      position: 2
+    }
+    var towers = [
+      {position: 5,height: 10,upright: false},
+      {position: 9,height: 9,upright: true},
+      {position: 14,height: 8,upright: false},
+      {position: 19,height: 12,upright: true},
+      {position: 23,height: 10,upright: false},
+      {position: 27,height: 11,upright: true},
+
+      {position: 33,height: 12,upright: false},
+      {position: 38,height: 9,upright: true},
+      {position: 41,height: 7,upright: false},
+      {position: 45,height: 10,upright: true},
+      {position: 50,height: 12,upright: false},
+      {position: 55,height: 11,upright: true},
+    ];
+    gird[bird.height][bird.position] = 'red';
+    this.state = {gird:gird, bird:bird, towers:towers, crashed:false, score: 0}
+
+    this.timerID = setInterval( ()=> {
+      if (this.state.crashed){
+        return;
       }
-      var bird = {
-        top: 250,
-        left: 20,
-        width: 40,
-        height: 40,
+      var girdCopy = [];
+      var towersCopy = this.state.towers.slice();
+
+      for (let i = 0; i < 30; i++) {
+        girdCopy.push(new Array(60).fill('#8b9dc3'));
       }
-    
-    
-    this.state = {gird:gird , bird:bird}
+
+      for (let i = 0; i < towersCopy.length; i++) {
+        towersCopy[i].position --;
+        if (towersCopy[i].position < 0){
+          towersCopy[i].position = 59;
+          towersCopy[i].height = Math.floor(Math.random() * 12) + 3;
+        }
+      }
+
+      for (let i = 0; i < towersCopy.length; i++) {
+        for (let j = 0; j < towersCopy[i].height; j++) {
+          if(towers[i].upright) {
+            girdCopy[29 - j][towersCopy[i].position] = '#ffdb01';  
+          }     
+          else {
+            girdCopy[j][towersCopy[i].position] = '#ffdb01';   
+          }
+        }
+      }
+      
+      var birdCopy = this.state.bird;
+      bird.height ++;
+      var crashed = false;
+      if(bird.height > 29){
+        bird.height = 10;
+        crashed = true;
+      }
+
+      for (let i = 0; i < 30; i++) {
+        if(girdCopy[i][2] === '#ffdb01' && birdCopy.height === i) {
+          birdCopy.height = 10 ;
+          crashed = true;
+        }
+      }
+      if(crashed) {
+        this.setState({crashed:true,score: 0})
+      }
+      girdCopy[birdCopy.height][birdCopy.position] = 'red';
+      this.setState({gird:girdCopy,bird:birdCopy, towers:towersCopy, score: this.state.score + 1});
+    }, 200);
   }
- 
-  render() {
+
+  handleClick() {
+    if(this.state.crashed) {
+      return;
+    }
+    var birdCopy = this.state.bird;
+    birdCopy.height -= 3;
+    this.setState({bird:birdCopy});
+  }
+  restart(){
+    this.setState({crashed:false,score: 0})
+  }
+  render(){
     return (
-      <Gird gird = {this.state.gird} bird = {this.state.bird}>
-      </Gird>
+      <div onClick = {this.handleClick.bind(this)}>
+        <Gird gird = {this.state.gird}/>
+        {this.state.crashed? <button onClick = {this.restart.bind(this)}>Press restart</button>: this.state.score}
+      </div>
     )
   }
 }
+
 export default Game;
+
